9fd09b58029de645b4b2a51c8553305547fafaef
Oleksandr Maslakov <oleksandr.maslakov@zazmic.com>
2022-04-02 10:59:45 +0300

    init

---
+++ src/config.js
@@ -0,0 +1,20 @@
+const { createClient } = require("@supabase/supabase-js");
+
+if (
+  !process.env.CONFIG ||
+  !process.env.SUPABASE_URL ||
+  !process.env.SUPABASE_KEY
+) {
+  throw new Error("Env vars: CONFIG, SUPABASE_URL, SUPABASE_KEY are required!");
+}
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+const config = require("../config.json"); //JSON.parse(process.env.CONFIG);
+
+const fetchChannelCount = parseInt(process.env.FETCH_CHANNELS_COUNT, 10) || 5;
+
+module.exports = { fetchChannelCount, config, supabase };
---
+++ src/funcs.js
@@ -0,0 +1,181 @@
+const { supabase, config, fetchChannelCount } = require("./config");
+const { parseByConfig } = require("tgspace-parser");
+
+// Fetch channel message last published at date
+const fetchMessageLastPublishedAt = async (channelId) => {
+  const { data: messages, error } = await supabase
+    .from("messages")
+    .select("published_at")
+    .order("published_at", { ascending: false })
+    .eq("channel_id", channelId)
+    .limit(1);
+
+  if (error) {
+    console.trace(error);
+    throw error;
+  }
+
+  if (messages.length > 0) {
+    return messages[0].published_at;
+  }
+
+  return `${new Date().getFullYear()}-01-01T00:00:00+00:00`;
+};
+
+// Fetch target channels
+const fetchTargetChannels = async (count = 10) => {
+  const { data: channels, error } = await supabase
+    .from("channels")
+    .select("id, tg_channel_id")
+    .order("updated_at", { ascending: true })
+    .limit(count);
+
+  if (error) {
+    console.trace(error);
+    throw error;
+  }
+
+  return channels;
+};
+
+// Update channel info
+const updateChannelInfo = async (tgChannelId, channelId) => {
+  const { title, image, description } = await parseByConfig({
+    context: {
+      tgChannelId,
+    },
+    target: "`https://t.me/${$c.tgChannelId}`",
+    parse: [
+      {
+        key: "title",
+        pick: [".tgme_page_title", "content"],
+      },
+      {
+        key: "image",
+        pick: [".tgme_page_photo_image", "attr", "src"],
+      },
+      {
+        key: "description",
+        pick: [".tgme_page_description", "content"],
+      },
+    ],
+  });
+
+  const { error } = await supabase
+    .from("channels")
+    .update({
+      name: title,
+      image,
+      info: description,
+      updated_at: "now()",
+    })
+    .match({ id: channelId });
+
+  if (error) {
+    console.trace(`Update channels error (${channelId}): `, error);
+    throw error;
+  }
+};
+
+// Insert message
+const insertMessage = async ({
+  tgMessageId,
+  channelId,
+  message,
+  publishedAt,
+  media,
+}) => {
+  const { error } = await supabase.from("messages").insert({
+    channel_id: channelId,
+    tg_message_id: tgMessageId,
+    message: message,
+    published_at: publishedAt,
+    media,
+  });
+
+  if (error) {
+    console.trace(`Update channels error (${channelId}): `, error);
+    throw error;
+  }
+};
+
+// Update channel info, fetch latest messages and insert to db
+const run = async () => {
+  const targetChannels = await fetchTargetChannels(fetchChannelCount);
+
+  for (let { id: channelId, tg_channel_id: tgChannelId } of targetChannels) {
+    const hrstart = process.hrtime();
+    const prefix = `[${tgChannelId}]`;
+
+    console.log(
+      `${prefix} START: tgChannelI: ${tgChannelId} channelId: ${channelId}`
+    );
+
+    // Parse and update channel info
+    await updateChannelInfo(tgChannelId, channelId);
+
+    const lastPublishedAt = await fetchMessageLastPublishedAt(channelId);
+
+    const parseConfig = {
+      ...config,
+      context: {
+        tgChannelId,
+        lastPublishedAt,
+      },
+    };
+
+    // Parse and insert new messages
+    for await (let { before, messages, lastPublishedAt } of parseByConfig(
+      parseConfig
+    )) {
+      for (let {
+        tgMessageId,
+        message,
+        images,
+        videos,
+        voices,
+        publishedAt,
+      } of messages) {
+        const media = [
+          ...images.map((image) => {
+            return {
+              type: "image",
+              ...image,
+            };
+          }),
+          ...videos.map((video) => {
+            return {
+              type: "video",
+              ...video,
+            };
+          }),
+          ...voices.map((voice) => {
+            return {
+              type: "voice",
+              ...voice,
+            };
+          }),
+        ];
+
+        await insertMessage({
+          tgMessageId,
+          channelId,
+          message,
+          publishedAt,
+          media,
+        });
+      }
+
+      console.info(`${prefix} before: ${before}`);
+      console.info(`${prefix} messages.length: ${messages.length}`);
+      console.info(`${prefix} lastPublishedAt: ${lastPublishedAt}`);
+    }
+
+    const hrend = process.hrtime(hrstart);
+    console.info(`${prefix} END: %ds %dms`, hrend[0], hrend[1] / 1000000);
+  }
+};
+
+module.exports = {
+  run,
+};
